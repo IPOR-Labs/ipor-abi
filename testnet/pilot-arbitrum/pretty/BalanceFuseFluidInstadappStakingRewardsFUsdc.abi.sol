a6c6ddaaad2134fdb7f20fe4b7c54d78e68aee2f
rav <rav@ipor.io>
2024-08-08 15:41:56 +0200

    IL-4643 Add Fluid fuses to pilot env

---
+++ testnet/pilot-arbitrum/pretty/BalanceFuseFluidInstadappStakingRewardsFUsdc.abi.sol
@@ -0,0 +1,5 @@
+interface FluidInstadappStakingBalanceFuse {
+    function MARKET_ID() external view returns (uint256);
+    function balanceOf(address plasmaVault_) external view returns (uint256);
+}
+
