c21b7189cd3a902fe0713d2669c93216d8971f69
rav <rav@ipor.io>
2024-11-20 20:43:25 +0100

    IL-5068 Add mainnet-base-tesseract

---
+++ chain-id/pretty/8453/0x56e00812a78158c405f5433246A0376719F0DA81.abi.sol
@@ -0,0 +1,33 @@
+interface WithdrawManager {
+    struct WithdrawRequestInfo {
+        uint256 amount;
+        uint256 endWithdrawWindowTimestamp;
+        bool canWithdraw;
+        uint256 withdrawWindowInSeconds;
+    }
+
+    error AccessManagedInvalidAuthority(address authority);
+    error AccessManagedRequiredDelay(address caller, uint32 delay);
+    error AccessManagedUnauthorized(address caller);
+    error InvalidInitialization();
+    error NotInitializing();
+    error SafeCastOverflowedUintDowncast(uint8 bits, uint256 value);
+    error WithdrawWindowLengthCannotBeZero();
+
+    event AuthorityUpdated(address authority);
+    event Initialized(uint64 version);
+    event ReleaseFundsUpdated(uint256 releaseFunds);
+    event WithdrawRequestUpdated(address account, uint256 amount, uint32 endWithdrawWindow);
+    event WithdrawWindowLengthUpdated(uint256 withdrawWindowLength);
+
+    function authority() external view returns (address);
+    function canWithdrawAndUpdate(address account_, uint256 amount_) external returns (bool);
+    function getWithdrawWindow() external view returns (uint256);
+    function isConsumingScheduledOp() external view returns (bytes4);
+    function releaseFunds() external;
+    function request(uint256 amount_) external;
+    function requestInfo(address account_) external view returns (WithdrawRequestInfo memory);
+    function setAuthority(address newAuthority) external;
+    function updateWithdrawWindow(uint256 window_) external;
+}
+
