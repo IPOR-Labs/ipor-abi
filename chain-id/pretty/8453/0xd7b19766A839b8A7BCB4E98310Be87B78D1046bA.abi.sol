d4d828cbcd4eaba1b8c24e6f839bd3185e078393
rav <rav@ipor.io>
2025-03-04 18:11:07 +0100

    IL-5492 Add ABI files

---
+++ chain-id/pretty/8453/0xd7b19766A839b8A7BCB4E98310Be87B78D1046bA.abi.sol
@@ -0,0 +1,18 @@
+interface AssetChainlinkPriceFeed {
+    error InvalidDecimals();
+    error MathOverflowedMulDiv();
+    error NegativeOrZeroPrice();
+    error SafeCastOverflowedIntToUint(int256 value);
+    error SafeCastOverflowedUintToInt(uint256 value);
+    error ZeroAddress();
+
+    function ASSET_X() external view returns (address);
+    function ASSET_X_ASSET_Y_CHAINLINK_FEED() external view returns (address);
+    function ASSET_Y_USD_CHAINLINK_FEED() external view returns (address);
+    function decimals() external pure returns (uint8);
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 price, uint256 startedAt, uint256 time, uint80 answeredInRound);
+}
+
