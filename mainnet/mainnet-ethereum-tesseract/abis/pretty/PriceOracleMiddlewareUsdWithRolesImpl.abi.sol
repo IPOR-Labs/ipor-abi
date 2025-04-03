faaa36b47ac6bf02faaba1f941fa86c45cf1635d
rav <85165079+rav-ipor@users.noreply.github.com>
2025-04-03 14:11:37 +0200

    IL-5630 Update addresses after wBTC Tesseract vault deployment (#114)
    * IL-5630 Update addresses after wBTC Tesseract vault deployment
    
    * IL-5630 Add ABI files
    
    * IL-5630 Update vaults and fuses list

--- mainnet/mainnet-ethereum-tesseract/abis/pretty/PriceOracleMiddlewareUsdWithRolesImpl.abi.sol
+++ mainnet/mainnet-ethereum-tesseract/abis/pretty/PriceOracleMiddlewareUsdWithRolesImpl.abi.sol
@@ -1,4 +1,6 @@
-interface PriceOracleMiddleware {
+interface PriceOracleMiddlewareWithRoles {
+    error AccessControlBadConfirmation();
+    error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
     error AddressEmptyCode(address target);
     error ArrayLengthMismatch();
     error AssetPriceSourceAlreadySet();
@@ -10,8 +12,6 @@ interface PriceOracleMiddleware {
     error InvalidExpectedPrice();
     error InvalidInitialization();
     error NotInitializing();
-    error OwnableInvalidOwner(address owner);
-    error OwnableUnauthorizedAccount(address account);
     error PriceDeltaTooHigh();
     error SafeCastOverflowedIntToUint(int256 value);
     error SourceAddressCanNotBeZero();
@@ -19,33 +19,46 @@ interface PriceOracleMiddleware {
     error UUPSUnsupportedProxiableUUID(bytes32 slot);
     error UnexpectedPriceResult();
     error UnsupportedAsset();
-    error WrongDecimals();
-    error ZeroAddress(string variableName);
 
     event AssetPriceSourceUpdated(address asset, address source);
     event Initialized(uint64 version);
-    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event NewPtPriceFeedDeployedEvent(address ptPriceFeed);
+    event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole);
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+    event RoleRevoked(bytes32 indexed role, address indexed account, address indexed sender);
     event Upgraded(address indexed implementation);
 
+    function ADD_PT_TOKEN_PRICE() external view returns (bytes32);
     function CHAINLINK_FEED_REGISTRY() external view returns (address);
+    function DEFAULT_ADMIN_ROLE() external view returns (bytes32);
     function QUOTE_CURRENCY() external view returns (address);
     function QUOTE_CURRENCY_DECIMALS() external view returns (uint256);
+    function SET_ASSETS_PRICES_SOURCES() external view returns (bytes32);
     function UPGRADE_INTERFACE_VERSION() external view returns (string memory);
-    function acceptOwnership() external;
+    function createAndAddPtTokenPriceFeed(
+        address pendleOracle_,
+        address pendleMarket_,
+        uint32 twapWindow_,
+        int256 expextedPriceAfterDeployment_,
+        uint256 usePendleOracleMethod_
+    ) external returns (address ptPriceFeed);
     function getAssetPrice(address asset_) external view returns (uint256 assetPrice, uint256 decimals);
     function getAssetsPrices(address[] memory assets_)
         external
         view
         returns (uint256[] memory assetPrices, uint256[] memory decimalsList);
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+    function getRoleMember(bytes32 role, uint256 index) external view returns (address);
+    function getRoleMemberCount(bytes32 role) external view returns (uint256);
     function getSourceOfAssetPrice(address asset_) external view returns (address sourceOfAssetPrice);
-    function initialize(address initialOwner_) external;
-    function owner() external view returns (address);
-    function pendingOwner() external view returns (address);
+    function grantRole(bytes32 role, address account) external;
+    function hasRole(bytes32 role, address account) external view returns (bool);
+    function initialize(address initialAdmin_) external;
     function proxiableUUID() external view returns (bytes32);
-    function renounceOwnership() external;
+    function renounceRole(bytes32 role, address callerConfirmation) external;
+    function revokeRole(bytes32 role, address account) external;
     function setAssetsPricesSources(address[] memory assets_, address[] memory sources_) external;
-    function transferOwnership(address newOwner) external;
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
     function upgradeToAndCall(address newImplementation, bytes memory data) external payable;
 }
 
