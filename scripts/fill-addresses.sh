897ab163408dadec934776901dbda6ed769eb4ef
rav <rav@ipor.io>
2025-04-17 18:01:49 +0200

    IL-5701 Add script fill-addresses.sh that will execute all filling addresses scripts

---
+++ scripts/fill-addresses.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DIRECTORIES=(
+  "fill-fuses"
+  "fill-prehooks"
+  "fill-price-feeds"
+  "fill-vaults"
+)
+
+for DIRECTORY in "${DIRECTORIES[@]}"; do
+  echo -e "\n================================================\n"
+  echo "Starting process in $DIRECTORY directory..."
+  cd "$SCRIPTS_DIR/$DIRECTORY"
+  
+  echo "Executing ./$DIRECTORY.py"
+  ./"$DIRECTORY.sh"
+
+  echo "Completed process in $DIRECTORY directory"
+done
+
+echo "All processes completed successfully"
