9852346c8fbe2b685c63622cf51d844cc7fde8f2
rav <rav@ipor.io>
2025-07-11 15:38:58 +0200

    IL-6117 Add ABI files

---
+++ chain-id/pretty/239/0x535595DBD2069eb4FE9378f69a6D64719c4ac9b7.abi.sol
@@ -0,0 +1,8 @@
+interface USDPriceFeed {
+    function decimals() external view returns (uint8);
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 price, uint256 startedAt, uint256 time, uint80 answeredInRound);
+}
+
